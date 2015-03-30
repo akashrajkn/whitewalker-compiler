83ccc3a77f37b81c9b48a867598e43235ab2bcb6
akashraj <akashrajkn@gmail.com>
2015-03-30 18:33:46 +0530

    more  keyword-states added

--- lexer.c
+++ lexer.c
@@ -35,7 +35,7 @@ enum tokenType
   TK_LP, TK_RP, TK_LB, TK_RB, TK_COM, TK_SEMICOL, TK_PLUS, TK_MINUS, TK_MUL,
   TK_LSB, TK_RSB, TK_COL, TK_DOL, TK_EQ, TK_NOT, TK_GT, TK_LT, TK_GET, TK_LET,
   TK_NEQ, TK_EQEQ, TK_WIF, TK_WINT, TK_WELSE, TK_WFLOAT, TK_ID, TK_WWIC, TK_WFDEF,
-  TK_WBOOL, TK_WTRUE, TK_WFLS
+  TK_WBOOL, TK_WTRUE, TK_WFLS, TK_WCHAR, TK_WHILE, TK_WRET
 
 }tokens;
 
@@ -137,13 +137,17 @@ int getState(char *ch)
 
         case 'f': { nextState = 7; break; }
 
-        case 'r': { nextState = 4; break; }
+        case 'e': { nextState = 19; break; }
 
-        case 's': { nextState = 5; break; }
+        case 'c': { nextState = 23; break; }
 
-        case 'w': { nextState = 1; break; }
+        case 'b': { nextState = 27; break; }
 
-        case 't': { nextState = 1; break; }
+        case 't': { nextState = 31; break; }
+
+        case 'w': { nextState = 35; break; }
+
+        case 'r': { nextState = 40; break; }
 
         case '=': { nextState = 1; break; }
 
@@ -427,8 +431,373 @@ int getState(char *ch)
 
     case 19:
     {
-      
+      if(*ch=='l')  // "el"
+      {
+        nextState = 20;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
     }
+
+    case 20:
+    {
+      if(*ch=='s')  //"els"
+      {
+        nextState = 21;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 21:
+    {
+      if(*ch=='e') //"else"
+      {
+        nextState = 22;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 22:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(6); //print the keyword "else"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+
+    case 23:
+    {
+      if(*ch=='h')  // "ch"
+      {
+        nextState = 24;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 24:
+    {
+      if(*ch=='a')  // "cha"
+      {
+        nextState = 25;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 25:
+    {
+      if(*ch=='r')  // "char"
+      {
+        nextState = 26;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 26:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(7); //print the keyword "char"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+
+    case 27:
+    {
+      if(*ch=='o')  // "bo"
+      {
+        nextState = 28;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 28:
+    {
+      if(*ch=='o')  // "boo"
+      {
+        nextState = 29;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 29:
+    {
+      if(*ch=='l')  // "bool"
+      {
+        nextState = 30;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 30:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(8); //print the keyword "bool"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+
+    case 31:
+    {
+      if(*ch=='r')
+      {
+        nextState = 32;  // "tr"
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 32:
+    {
+      if(*ch=='u')  // "tru"
+      {
+        nextState = 33;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 33:
+    {
+      if(*ch=='e')  // "true"
+      {
+        nextState = 34;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 34:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(9); //print the keyword "true"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+
+    case 35:
+    {
+      if(*ch=='h')  // "wh"
+      {
+        nextState = 36;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 36:
+    {
+      if(*ch=='i')  // "whi"
+      {
+        nextState = 37;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 37:
+    {
+      if(*ch=='l')  // "whil"
+      {
+        nextState = 38;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 38:
+    {
+      if(*ch=='e')  // "while"
+      {
+        nextState = 39;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 39:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(10); //print the keyword "true"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+
+    case 40:
+    {
+      if(*ch=='e')   // "re"
+      {
+        nextState = 41;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 41:
+    {
+      if(*ch=='t')  // "ret"
+      {
+        nextState = 42;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 42:
+    {
+      if(*ch=='u')  // "retu"
+      {
+        nextState = 43;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }  
+
+    case 42:
+    {
+      if(*ch=='r') // "retur"
+      {
+        nextState = 43;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 43:
+    {
+      if(*ch=='n')  // "return"
+      {
+        nextState = 44;
+        break;
+      }
+      else
+      {
+        if(checkID(ch)){ nextState = 4;retract(); break; } else{ nextState = 5; break; }
+      }
+    }
+
+    case 44:
+    {
+      if(checkID(ch))
+      {
+        retract();
+        printKeyword(11); //print the keyword "return"
+        nextState = 0;
+        break;
+      }
+      else
+      {
+        nextState = 5;
+        break;
+      }
+    }
+    
 
   }//switch statement for state variable ends here
 
@@ -449,6 +818,18 @@ void printKeyword(int choice)
     case 4: fprintf(fout, "TK_WFDEF      %d        fdef\n", TK_WFDEF); break;
 
     case 5: fprintf(fout, "TK_WFLS       %d        false\n", TK_WFLS); break;
+
+    case 6: fprintf(fout, "TK_WELSE      %d        else\n", TK_WELSE); break;
+
+    case 7: fprintf(fout, "TK_WCHAR      %d        char\n", TK_WCHAR); break;
+
+    case 8: fprintf(fout, "TK_WBOOL      %d        bool\n", TK_WBOOL); break;
+
+    case 9: fprintf(fout, "TK_WTRUE      %d        true\n", TK_WTRUE); break;
+
+    case 10: fprintf(fout, "TK_WHILE      %d        while\n", TK_WHILE); break;
+
+    case 11: fprintf(fout, "TK_WRET       %d        return\n", TK_WRET); break;
 
 
   }
